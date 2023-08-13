bd3e9e4cc29b9b105f6a06a1059b17537596d293
Dawid Lechowski <david.lechowski@gmail.com>
2023-08-13 13:46:14 +0200

    add commonly used types

--- types/types.ts
+++ types/types.ts
@@ -1,3 +1,23 @@
+export interface User {
+  email: String;
+  firstName: String;
+  id: string;
+  lastName: string;
+  role: string;
+}
+
+export interface SingleRoom {
+  id: string;
+  name: string;
+}
+
+export interface Message {
+  id: string;
+  body: string;
+  insertedAt: string;
+  user: User;
+}
+
 export type RootStackParamList = {
   Home: undefined;
   Chat: {
