dd540b99e9a4ec18bb6be80dc3162a40c830e682
Dawid Lechowski <david.lechowski@gmail.com>
2023-08-13 13:24:25 +0200

    add Poppins fonts

--- App.tsx
+++ App.tsx
@@ -1,7 +1,14 @@
 import { AppRegistry } from 'react-native';
+import { useFonts } from 'expo-font';
 import { View } from 'react-native';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'Poppins-Light': require('./assets/fonts/Poppins-Light.ttf'),
+    'Poppins-Medium': require('./assets/fonts/Poppins-Medium.ttf'),
+    'Poppins-SamiBold': require('./assets/fonts/Poppins-SemiBold.ttf'),
+    'Poppins-Bold': require('./assets/fonts/Poppins-Bold.ttf'),
+  });
   return <View></View>;
 }
 
